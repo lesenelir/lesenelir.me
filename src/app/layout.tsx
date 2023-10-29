6e69aec795b58b52b0019c0f2780823533322b05
lesenelir <hi@lesenelir.me>
2023-10-29 21:18:24 +0800

    feat: client component to handle route switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import type { Metadata } from 'next'
+import Link from 'next/link'
 
 import Footer from '@/components/ui/Footer'
 import Divide from '@/components/utils/Divide'
+import HeaderClient from '@/components/client/HeaderClient'
 
 import './globals.css'
-import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Lesenelir 3000',
@@ -20,40 +21,43 @@ export default function RootLayout(props: IProps) {
   const {children} = props
 
   return (
-    <html lang="en">
-    <body className="bg-gray-100 min-h-screen font-sans">
-      <div className="max-w-screen-md my-20 mx-auto px-6 max-md:px-4">
-        {/* Header */}
-        <header className="pb-4 mb-8">
-          <h1 className="text-3xl font-comic text-black dark:text-white">
-            <Link href={'/'}>Lesenelir Zhou</Link>
-          </h1>
-          <ul className="flex flex-row justify-end gap-4 text-navbarLight underline underline-offset-1 dark:text-navbarDark">
-            <li className="header-li">
-              <Link href={'/posts'}>Posts</Link>
-            </li>
-            <li className="header-li">
-              <Link href={'/projects'}>Projects</Link>
-            </li>
-            <li className="header-li">
-              <Link href={'/photos'}>Photos</Link>
-            </li>
-          </ul>
-        </header>
+    <html lang='en'>
+      <body suppressHydrationWarning={true} className={'bg-gray-100 min-h-screen font-sans'}>
+        <div className={'max-w-screen-md my-20 mx-auto px-6 max-md:px-4'}>
+          {/* Header */}
+          <header className={'pb-4 mb-8'}>
+            <h1 className={'text-3xl font-comic text-black dark:text-white'}>
+              <HeaderClient/>
+            </h1>
+            <ul className={'header-ul'}>
+              <li className={'header-li'}>
+                <Link href={'/'}>Home</Link>
+              </li>
+              <li className={'header-li'}>
+                <Link href={'/posts'}>Posts</Link>
+              </li>
+              <li className={'header-li'}>
+                <Link href={'/projects'}>Projects</Link>
+              </li>
+              <li className={'header-li'}>
+                <Link href={'/photos'}>Photos</Link>
+              </li>
+            </ul>
+          </header>
 
-        {/* Content */}
-        <article className="mb-8 text-textLight dark:text-textDark">
-          {children}
-        </article>
+          {/* Content */}
+          <article className="mb-8 text-textLight dark:text-textDark">
+            {children}
+          </article>
 
-        <Divide/>
+          <Divide/>
 
-        {/* Footer */}
-        <footer className="pt-4">
-          <Footer/>
-        </footer>
-      </div>
-    </body>
+          {/* Footer */}
+          <footer className="pt-4">
+            <Footer/>
+          </footer>
+        </div>
+      </body>
     </html>
   )
 }
---
+++ src/components/client/HeaderClient.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { usePathname } from 'next/navigation'
+
+export default function HeaderClient() {
+  const pathname = usePathname()
+
+  if (pathname === '/') {
+    return (
+      <>Lesenelir Zhou</>
+    )
+  }
+
+  if (pathname === '/posts') {
+    return (
+      <>Posts</>
+    )
+  }
+
+  if (pathname === '/projects') {
+    return (
+      <>Projects</>
+    )
+  }
+
+  if (pathname === '/photos') {
+    return (
+      <>Photos</>
+    )
+  }
+
+  return null
+}
