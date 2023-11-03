14db3404c707e81aad6a4c3967f0b1e1428eee96
lesenelir <hi@lesenelir.me>
2023-11-03 14:31:46 +0800

    fix: Link component not working in client component in production environment, and splitting components to separate client-side and server-side

--- src/components/header/NavbarList.tsx
+++ src/components/header/NavbarList.tsx
@@ -1,9 +1,14 @@
 'use client'
 
-import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import NavbarServerComponent from '@/components/header/NavbarServerComponent'
 
-export const headerList: {name: string, path: string}[] = [
+export type THeaderList = {
+  name: string
+  path: string
+}
+
+export const headerList: THeaderList[] = [
   {
     name: 'Home',
     path: '/',
@@ -33,22 +38,33 @@ export default function NavbarList() {
     <>
       {
         headerList.map(item => (
-          <li
+          <NavbarServerComponent
             key={item.name}
-            className={`header-li ${pathname === item.path ? '' : 'header-li-unActive'}`}
-          >
-            {
-              pathname === item.path ? (
-                <p>{item.name}</p>
-              ) : (
-                <Link prefetch={false} href={item.path}>
-                  {item.name}
-                </Link>
-              )
-            }
-          </li>
+            item={item}
+            isActive={pathname === item.path}
+          />
         ))
       }
+
+      {/* Client Component Logic */}
+      {/*{*/}
+      {/*  headerList.map(item => (*/}
+      {/*    <li*/}
+      {/*      key={item.name}*/}
+      {/*      className={`header-li ${pathname === item.path ? '' : 'header-li-unActive'}`}*/}
+      {/*    >*/}
+      {/*      {*/}
+      {/*        pathname === item.path ? (*/}
+      {/*          <p>{item.name}</p>*/}
+      {/*        ) : (*/}
+      {/*          <Link prefetch={false} href={item.path}>*/}
+      {/*            {item.name}*/}
+      {/*          </Link>*/}
+      {/*        )*/}
+      {/*      }*/}
+      {/*    </li>*/}
+      {/*  ))*/}
+      {/*}*/}
     </>
   )
 }
---
+++ src/components/header/NavbarServerComponent.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+import type { THeaderList } from '@/components/header/NavbarList'
+
+interface IProps {
+  item: THeaderList
+  isActive: boolean
+}
+
+export default function NavbarServerComponent(props: IProps) {
+  const {item, isActive} = props
+
+  return (
+    <>
+      <li className={`header-li ${isActive ? '' : 'header-li-unActive'}`}>
+        {
+          isActive ? (
+            <p>{item.name}</p>
+          ) : (
+            <Link prefetch={false} href={item.path}>
+              <p>{item.name}</p>
+            </Link>
+          )
+        }
+      </li>
+    </>
+  )
+}
