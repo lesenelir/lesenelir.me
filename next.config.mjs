29b9d92919f863001b26818b04edaa65c6741c1e
lesenelir <hi@lesenelir.me>
2023-11-04 15:39:29 +0800

    feat: rss

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,24 @@ const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-  reactStrictMode: true
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/rss',
+        destination: '/feed.xml',
+      },
+      {
+        source: '/rss.xml',
+        destination: '/feed.xml',
+      },
+      {
+        source: '/feed',
+        destination: '/feed.xml',
+      },
+    ]
+  }
 }
 
 export default withMDX(nextConfig)
