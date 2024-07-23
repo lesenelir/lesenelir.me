4a7e33361a7fb4c4fc4c1b3103ebefbb66d03a67
lesenelir <hi@lesenelir.me>
2024-07-23 14:12:51 +0800

    feat: setup prettier config

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,49 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-sort-json',
+    'prettier-plugin-packagejson',
+    'prettier-plugin-tailwindcss' // MUST come last
+  ],
+  // options for prettier-plugin-sort-json
+  ...{
+    jsonRecursiveSort: true,
+  },
+  // options for prettier-plugin-tailwindcss
+  ...{
+    tailwindFunctions: ['cva', 'cn'],
+  },
+  // options for @ianvs/prettier-plugin-sort-imports
+  importOrder: [
+    '^react$',
+    '',
+
+    '<THIRD_PARTY_MODULES>',
+    '<TYPES>',
+    '',
+
+    '^@/',
+    '<TYPES>^@/',
+    '',
+
+    '^~/',
+    '<TYPES>^~/',
+    '',
+
+    '^[.]',
+    '<TYPES>^[.]',
+    '',
+
+    '^(?!.*[.]css$)[./].*$',
+    '.css$',
+    '',
+  ]
+}
