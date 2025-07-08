9370d68c59addcb803c01676871d6bdbb96fce59
lesenelir <hi@lesenelir.me>
2025-07-08 17:21:29 +0800

    feat: configure Vitest with path alias for improved module resolution

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,4 @@
+// Named exports for specific configurations
+export { default as base } from './base.js'
+export { default as next } from './next.js'
+export { default as reactInternal } from './react-internal.js'
