ac71ffc30ec2d07a1b4e95905e03e61fd705a791
lesenelir <hi@lesenelir.me>
2025-07-12 02:14:22 +0800

    fix: mobile sidebar layout

--- apps/website/src/components/common/sidebar.tsx
+++ apps/website/src/components/common/sidebar.tsx
@@ -13,7 +13,7 @@ export function Sidebar() {
     <aside
       className={cn(
         'text-text-foreground font-cursive',
-        'fixed z-50 max-sm:right-6 max-sm:top-6 sm:left-16 sm:top-16',
+        'z-50 max-sm:absolute max-sm:right-6 max-sm:top-6 sm:fixed sm:left-16 sm:top-16',
         'flex h-auto gap-3 sm:flex-col'
       )}
     >
