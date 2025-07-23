52f8f0d236a32a5ccf4b809275a376522359fb6c
lesenelir <hi@lesenelir.me>
2025-07-23 14:47:12 +0800

    chore: rename photo-data and song-data files for consistency

--- apps/website/src/constants/index.ts
+++ apps/website/src/constants/index.ts
@@ -1,4 +1,4 @@
 export * from './sidebar-data'
 export * from './posts-metadata'
-export * from './photo-data'
-export * from './song-data'
+export * from './photos-data'
+export * from './songs-data'
